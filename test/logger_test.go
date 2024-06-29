7cae47ad4886f5d15fa7f82d9918e2a38f26d8e6
Grbisba <polickarpow.dany@yandex.ru>
2024-06-29 22:50:14 +0300

    add test

--- test/logger_test.go
+++ test/logger_test.go
@@ -1,9 +1,35 @@
 package test
 
 import (
+	myLogger "github.com/package/logger"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 	"testing"
 )
 
+type Config struct {
+	Service    *string
+	InstanceID *string
+}
+
+func TestReplace(t *testing.T) {
+	before := zap.L()
+	log, err := myLogger.NewProduction()
+	require.NoError(t, err)
+	if assert.NotNil(t, log) {
+		after := zap.L()
+		assert.Equal(t, after, log)
+		assert.NotEqual(t, after, before)
+	}
+}
+
 func TestLogger(t *testing.T) {
-	
+	cfg := Config{
+		Service:    nil,
+		InstanceID: nil,
+	}
+	logger, err := myLogger.NewWithConfig(cfg)
+	require.NoError(t, err)
+	logger.Info("hello world", zap.String("key", "val"))
 }
